d2812b44516f49f94a7c3b056aa0e6f8d65845af
Rini Roscoe <rini.roscoe@gmail.com>
2014-05-27 00:09:43 -0500

    Initial Upload
    Now perhaps someday for a user interface... and the creation of
    "parties" rather than "collections of NPCs"... and for that matter,
    another look at the AI might yield some results...

---
+++ NPC_generator.py
@@ -0,0 +1,11 @@
+from elvenfire.creatures.character import PlayerCharacter
+
+def main():
+
+    numchars = 30
+    for i in range(numchars):
+        print(PlayerCharacter())
+
+
+if __name__ == '__main__':
+    main()
